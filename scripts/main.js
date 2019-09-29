8fea4499e440da4d037fea185e54d5ef78dfc859
Richarlotte <charlotteadolfssonolander@gmail.com>
2019-09-29 12:53:03 +0200

    added storyContainer

--- scripts/main.js
+++ scripts/main.js
@@ -23,34 +23,53 @@ function playCheckedAudio(){
     $("#selectionSound").prop("volume", 0.1);
 }
 
-// Play sound during storywriting
+// Play sound during storywriting and add storyContainer to the page
 $(".ready-button").click(function(){
     $("#magic-wand")[0].currentTime = 0;
     $("#magic-wand")[0].play();
     $("#magic-wand").prop("volume", 0.1);
+    $("#storyContainer").css("display", "flex");
 });
 
 // Get the input value
 
 
-var firstParOfStory = ""; 
-var secondPart = "";
+var firstPart = "Once upon a time there was a "; 
+var secondPart = " She was dreaming about living in the ";
+var thirdPart = " And brought what she always brings, the ";
 
 function writeStory(myOption){
+        
+        document.getElementById("result").innerHTML = "";
         var character = document.getElementsByName("character"); 
           
         for(i = 0; i < character.length; i++) { 
             if(character[i].type="radio") { 
                 if(character[i].checked) 
-                    console.log(firstParOfStory = " Once upon a time there was a " + character[i].value);
-            } 
+                document.getElementById("result").innerHTML
+                    += firstPart 
+                    + character[i].value;
+            }
         } 
         var enviroment = document.getElementsByName("enviroment"); 
-          
+
         for(j = 0; j < enviroment.length; j++) { 
             if(enviroment[j].type="radio") { 
                 if(enviroment[j].checked) 
-                    console.log(secondPart = " She was walking around in the " + enviroment[j].value);
+                    document.getElementById("result").innerHTML
+                    += secondPart 
+                    + enviroment[j].value;
+                  
+            } 
+        }
+        var item = document.getElementsByName("item");
+
+        for(e = 0; e < enviroment.length; e++) { 
+            if(enviroment[e].type="radio") { 
+                if(enviroment[e].checked) 
+                document.getElementById("result").innerHTML
+                += thirdPart 
+                + item[e].value;
             } 
         }
     } 
